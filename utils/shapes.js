0b035ecb15bda155f29ac70984bcc1dee0857eb1
nreece6 <reece.nick24@gmail.com>
2023-05-13 11:33:52 -0500

    built constructors for shapes

--- utils/shapes.js
+++ utils/shapes.js
@@ -1,3 +1,29 @@
 class Shape {
     // sets class of Shape
+    constructor(){
+        this.color = ''
+    }
+    setColor(color){
+        this.color=(color)
+    }
 }
+
+class Circle extends Shape {
+    render(){
+        return ``
+    }
+}
+
+class Square extends Shape {
+    render() {
+        return ``
+    }
+}
+
+class Triangle extends Shape {
+    render() {
+        return ``
+    }
+}
+
+module.exports = {Circle, Square, Triangle}
